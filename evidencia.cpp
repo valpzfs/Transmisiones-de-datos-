045adfe4cd577afda19dcb58fe07c9b821530535
Amparo <amparoalcarazt@gmail.com>
2025-10-07 11:25:00 -0600

    Lee archivos
    cambios a main para que lea los archivos txt

--- evidencia.cpp
+++ evidencia.cpp
@@ -7,20 +7,30 @@ using namespace std;
 
 int main() {
     for(int i=0;i<3;i++){
-        //string num= to_string(i+1);
-        //string filename ="transmission"+ num + ".txt";
-        // ifstream input(filename);
-        // if (!input.is_open()) {
-        //     cout<<"Error al abrir "<<filename<<endl;
-        // }else{
-        //     cout<<"Se abrio "<<filename<< endl;
-        //     // string line;
-        //     // while (getline(input, line)) {
-        //     //     cout << line << endl;
-        //     // }
-        // }
+        string num;
+        //Hay que cambiar esta seccion por algo que combierta i a string
+        if(i+1==1){
+            num="1";
+        }else if(i+1==2){
+            num="2";
+        }else if(i+1==3){
+            num="3";
+        }
+        //string num = to_string(i + 1);
+        string filename ="transmission";
+        filename+=num;
+        filename+=".txt";
+        ifstream input(filename);
+        if (!input.is_open()) {
+            cout<<"Error al abrir "<<"transmission"<<endl;
+        }else{
+            cout<<"Se abrio "<<filename<< endl;
+            string line;
+            while (getline(input, line)) {
+                cout << line << endl;
+            }
+            input.close();
+        }
     }
     return 0;
 }
-//g++ -std=c++11 main.cpp -o main
-//g++ -std=c++17 -o test evidencia.cpp
