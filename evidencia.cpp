0d0b965b80200d9a9fc95c972c407ff89fda1010
Amparo <amparoalcarazt@gmail.com>
2025-10-07 10:41:01 -0600

    Archivos base para proyecto
    Agregue los archivos que se nos dieron de ejemplo como base para el proyecto, + archivo cpp

---
+++ evidencia.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+    for(int i=0;i<3;i++){
+        //string num= to_string(i+1);
+        //string filename ="transmission"+ num + ".txt";
+        // ifstream input(filename);
+        // if (!input.is_open()) {
+        //     cout<<"Error al abrir "<<filename<<endl;
+        // }else{
+        //     cout<<"Se abrio "<<filename<< endl;
+        //     // string line;
+        //     // while (getline(input, line)) {
+        //     //     cout << line << endl;
+        //     // }
+        // }
+    }
+    return 0;
+}
+//g++ -std=c++11 main.cpp -o main
+//g++ -std=c++17 -o test evidencia.cpp
