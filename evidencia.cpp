b2b80d51901280e0a64fbc25fa19025085c49ed1
Amparo <amparoalcarazt@gmail.com>
2025-10-08 14:14:10 -0600

    KMP + outputs como ejemplo

--- evidencia.cpp
+++ evidencia.cpp
@@ -6,6 +6,57 @@
 using namespace std;
 
 //Complejidad O(n)
+void constructLps(string &pat, vector<int> &lps) {
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+    while (i < pat.length()) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else {
+            if (len != 0) {
+                len = lps[len - 1];
+            }else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+//Complejidad O(n+m)
+vector<int> KMPsearch(string &pat, string &txt) {
+    int n = txt.length();
+    int m = pat.length();
+    vector<int> lps(m);
+    vector<int> res;
+    constructLps(pat, lps);
+    int i = 0;
+    int j = 0;
+    while (i < n) {
+        if (txt[i] == pat[j]) {
+            i++;
+            j++;
+            if (j == m) {
+                res.push_back(i - j);
+                j = lps[j - 1];
+            }
+        }
+        else {
+            if (j != 0){
+                j = lps[j - 1];
+            }else{
+                i++;
+            }
+        }
+    }
+    return res;
+}
+
+//Complejidad O(n)
 string longestPalindrome(string s, int &start) {
     string T = "^";
     for (char c : s) {
@@ -46,27 +97,85 @@ string longestPalindrome(string s, int &start) {
     return s.substr(start,maxLen);
 }
 
-string KMP(){
-    return"";
+void PalindromeOutput(vector<string> transmissions, string &outputText){
+    string mirrorCodes;
+    outputText += "============\n";
+    outputText += "Palindromo mas grande \n";
+     for (int i=0;i<transmissions.size();i++){
+        outputText += "transmission"+ to_string(i+1) + ".txt ==> ";
+        int positionHolder;
+        mirrorCodes=longestPalindrome(transmissions[i], positionHolder);
+        outputText += "Posicion: "+ to_string(positionHolder) + "\n";
+        outputText += mirrorCodes + "\n";
+        
+    }
+    
+    outputText += "============\n";
 }
+
+//Complejidad O(n*m)
+void findSubsequences(vector<string> transmissions, string mcode, string &outputText){
+    string bestSubseq;
+    int bestFile;
+    int mostTimes=0;
+    vector<int> holder;
+    //cout<<mcode<<endl;
+    for (int i=0; i<mcode.size(); i++){
+        string sub=mcode;
+        sub.erase(i, 1);
+        //cout<<"subseq  "<<sub<<endl;
+        for(int t=0;t<transmissions.size();t++){
+            holder=KMPsearch(sub, transmissions[t]);
+            if(holder.size()>mostTimes){
+                mostTimes=holder.size();
+                bestFile=t;
+                bestSubseq=sub;
+            }
+        }   
+    }
+    outputText+="La subsecuencia más encontrada es: " +bestSubseq +" con "+ to_string(mostTimes) +" veces en el archivo" + to_string(bestFile) +".txt\n";
+}
+
+//Complejidad O(n*m)
+void findCodes(vector<string> transmissions, vector<string>mcodes, string &outputText){
+    string maxFound;
+    int timesFound=0;
+    for(int i=0;i<mcodes.size();i++){
+        outputText += "Código: " + mcodes[i]+" \n";
+        for(int j=0;j<transmissions.size();j++){
+            vector<int> res = KMPsearch(mcodes[i], transmissions[j]);
+            outputText += "transmission"+ to_string(j+1) + ".txt ==> ";
+            outputText += to_string(res.size()) + " veces\n";
+            for(int i=0;i<res.size();i++){
+                outputText+= to_string(res[i]);
+                if(i<res.size()-1){
+                    outputText+=", ";
+                }
+            }
+            outputText+= "\n";
+        }
+        findSubsequences(transmissions, mcodes[i], outputText);
+        
+        outputText+= "--------------\n \n";
+    }
+}
+
 
 
 int main() {
     vector<string> transmissions;
+    vector<string> mcodes;
     vector <string> mirrorCodes;
+    string filename;
+    string outputText="";
+
+    //TRANSMISSION FILES
     for(int i=0;i<3;i++){
         string num;
-        //Hay que cambiar esta seccion por algo que combierta i a string
-        if(i+1==1){
-            num="1";
-        }else if(i+1==2){
-            num="2";
-        }else if(i+1==3){
-            num="3";
-        }
+        num=to_string(i+1);
 
         //string num = to_string(i + 1);
-        string filename ="transmission";
+        filename ="transmission";
         filename+=num;
         filename+=".txt";
         ifstream input(filename);
@@ -81,13 +190,39 @@ int main() {
             input.close();
         }
     }
-    for (int i=0;i<transmissions.size();i++){
-        //cout<<transmissions[i]<<endl;
-        int positionHolder;
-        mirrorCodes.push_back(longestPalindrome(transmissions[i], positionHolder));
-        cout<<mirrorCodes[i]<<endl;
-        cout<<positionHolder<<endl;
+
+    //MALICIOUS CODES
+    filename="mcode.txt";
+    ifstream input(filename);
+    if (!input.is_open()) {
+        cout<<"Error al abrir "<<filename<<endl;
+    }else{
+        cout<<"Se abrio "<<filename<< endl;
+        string line;
+        while (getline(input, line)) {
+            mcodes.push_back(line);
+        }
+        input.close();
     }
 
+    //FIND PALINDROMES
+    
+
+
+    //FIND MALICIOUS CODES
+    findCodes(transmissions,mcodes, outputText);
+    PalindromeOutput(transmissions,outputText);
+
+    //OUTPUT
+    ofstream outputFile("checking0.txt");
+    if (outputFile.is_open()) {
+        outputFile << outputText;
+        cout<<outputText<<endl;
+
+        outputFile.close();
+        cout << "Se escribio en el achivo de output";
+    } else {
+        cout<< "No se pudo escribir en el achivo de output";
+    }
     return 0;
 }
